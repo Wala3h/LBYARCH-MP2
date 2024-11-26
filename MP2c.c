9ec9f2d1899e5ceb44a6c0a7e3da3c344d93eaa0
Peter Tan <136557371+Wala3h@users.noreply.github.com>
2024-11-26 17:17:46 +0800

    Execution time, 2 input matrices (10x10, 100x100)

--- MP2c.c
+++ MP2c.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 extern int  imgCvtGrayDoubleToInt(float floatVal);	
 
@@ -10,22 +12,27 @@ int main(){
 	int row, col;
 	printf("Input 2 ints (row, col): ");
 	scanf("%d %d", &row, &col);
-   
+	printf("\n");
+   printf("%d %d\n", row, col);
 	printf("Input matrix values (per row): \n");
 	float floatVec[row][col];
 	int intVec[row][col];
 	int i = 0;
+	
+	// start time counting (processing focused, eliminates input delay)
+	clock_t begin = clock();
 	
 	for (i; i < row; i++){
 		int j = 0;
 		
 		for (j; j < col; j++) {
 			scanf("%f", &floatVec[i][j]);
+			printf("%.2f ", floatVec[i][j]);
 			intVec[i][j] =  imgCvtGrayDoubleToInt(floatVec[i][j]);
 		}
-		printf("\n");
+		 printf("\n");
 	}
-
+	printf("\n");
 	// print output
 	int x = 0;
 	for (x; x < row; x++){
@@ -36,6 +43,9 @@ int main(){
 		}
 		printf("\n");
 	}
+	
+	clock_t end = clock();
+	double execTime = (double)(end-begin) / CLOCKS_PER_SEC;
+	printf("\nProcessing time (in seconds): %f", execTime);
    return 0;
 }
-
