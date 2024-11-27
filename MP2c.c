908f0b7b03d5e05b8b0c4ee4b4c5b5238688dc5a
Peter Tan <136557371+Wala3h@users.noreply.github.com>
2024-11-27 16:21:02 +0800

    Updated Implementation, inp sizes text files
    Passes array now instead of each float value, fast performance (will put in readme)

--- MP2c.c
+++ MP2c.c
@@ -6,7 +6,7 @@
 #include <time.h>
 
 
-extern int  imgCvtGrayDoubleToInt(float floatVal);	
+extern void imgCvtGrayDoubleToInt(int n, float* floatVec, int* intVec);	
 
 int main(){
 	int row, col;
@@ -15,36 +15,36 @@ int main(){
 	printf("\n");
    printf("%d %d\n", row, col);
 	printf("Input matrix values (per row): \n");
-	float floatVec[row][col];
-	int intVec[row][col];
-	int i = 0;
-	
+	int n = row*col;
+	float* floatVec= (float*)malloc(n*sizeof(float));
+	int* intVec = (int*)malloc(n*sizeof(int));
 	// start time counting (processing focused, eliminates input delay)
-	clock_t begin = clock();
-	
+	int i = 0;
 	for (i; i < row; i++){
 		int j = 0;
-		
 		for (j; j < col; j++) {
-			scanf("%f", &floatVec[i][j]);
-			printf("%.2f ", floatVec[i][j]);
-			intVec[i][j] =  imgCvtGrayDoubleToInt(floatVec[i][j]);
+			scanf("%f", &floatVec[i*col+j]);
+			printf("%.2f ", floatVec[i*col+j]);
 		}
-		 printf("\n");
+		printf("\n");
 	}
 	printf("\n");
+	
+	clock_t begin = clock();
+	imgCvtGrayDoubleToInt(n, floatVec, intVec);
+	clock_t end = clock();
 	// print output
 	int x = 0;
 	for (x; x < row; x++){
 		int y = 0;
 		
 		for (y; y < col; y++) {
-			printf("%d ", intVec[x][y]);
+			printf("%d ", intVec[x*col+y]);
 		}
 		printf("\n");
 	}
+	// clock_t end = clock();
 	
-	clock_t end = clock();
 	double execTime = (double)(end-begin) / CLOCKS_PER_SEC;
 	printf("\nProcessing time (in seconds): %f", execTime);
    return 0;
--- MP2sasm.asm
+++ MP2sasm.asm
@@ -8,8 +8,15 @@ bits 64
 default rel
 global imgCvtGrayDoubleToInt
 imgCvtGrayDoubleToInt:
-	; params: floatVec[i][j] = xmm0
+	; params: n = rcx, floatVec = rdx, intVec = r8
+        movss xmm0, [rdx]
 	movss xmm4, [const]
 	mulss xmm0, xmm4
 	cvtss2si rax, xmm0
+        mov [r8], rax
+        add rdx, 4
+        add r8, 4
+        dec rcx
+        cmp rcx, 0
+        jnz imgCvtGrayDoubleToInt
 ret
